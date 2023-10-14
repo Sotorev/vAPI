32626f372f0500ad631bb15c2213a6fc5b247621
Sotorev <sotorev7@gmail.com>
2023-10-14 11:30:27 -0600

    Add routes

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,27 @@ Route::get('/plates', 'PlateController@get');
 
 // Ruta para encontrar una placa por ID
 Route::get('/placas/{id}', 'PlateController@show');
+
+// Ruta para crear una placa
+Route::post('/placas', 'PlateController@store');
+
+// Ruta para actualizar una placa
+Route::put('/placas/{id}', 'PlateController@update');
+
+// Ruta para eliminar una placa
+Route::delete('/placas/{id}', 'PlateController@delete');
+
+// Rutas para listar todos los vehículos
+Route::get('/vehicles', 'VehicleController@get');
+
+// Ruta para encontrar un vehículo por ID
+Route::get('/vehicles/{id}', 'VehicleController@show');
+
+// Ruta para crear un vehículo
+Route::post('/vehicles', 'VehicleController@store');
+
+// Ruta para actualizar un vehículo
+Route::put('/vehicles/{id}', 'VehicleController@update');
+
+// Ruta para eliminar un vehículo
+Route::delete('/vehicles/{id}', 'VehicleController@delete');
